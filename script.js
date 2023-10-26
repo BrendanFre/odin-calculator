631d7ec7090fd7f52c9456892482ea1f165ccb81
Brendan Freeman <80400320+BrendanFre@users.noreply.github.com>
2023-10-26 21:19:13 +0000

    Finished calculator

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
-const button7 = document.querySelector('#seven')
-const button8 = document.querySelector('#eight')
-const button9 = document.querySelector('#nine')
 const theNumberButtons = document.querySelectorAll('.numberButton')
-const buttonDivide = document.querySelector('#divide')
+const theOperatorButtons = document.querySelectorAll('.operatorButton')
+const theEqualsButton = document.querySelector('#equalsButton')
+const calcDisplay = document.querySelector('#display')
+const clearButton = document.querySelector('#clear')
+
+
 let firstNumber = ""
 let secondNumber = ""
+let theOperator = ""
 let isSecondNumber = false
 
 theNumberButtons.forEach(item =>{item.addEventListener('click', displayNumber)})
+theOperatorButtons.forEach(item =>{item.addEventListener('click', displayOperator)})
+theEqualsButton.addEventListener('click', getTotal)
+clearButton.addEventListener('click', doClear)
+
+theOperatorButtons.forEach(item =>{item.classList.add('operators')})
+clearButton.classList.add('clearing')
+
+
 
 function displayNumber(number){
     if(isSecondNumber){
         secondNumber += number.target.value.toString(16)
+        calcDisplay.textContent = secondNumber
     } else{
         firstNumber += number.target.value.toString(16)
+        calcDisplay.textContent = firstNumber
     }
-    console.log(firstNumber)
+}
+
+function displayOperator(operator){
+    theOperator = operator.target.value.toString(16)
+    calcDisplay.textContent = theOperator
+    isSecondNumber = true
+    
+}
+
+function getTotal(){
+    firstNumber = parseInt(firstNumber)
+    secondNumber = parseInt(secondNumber)
+    console.log(firstNumber, secondNumber, theOperator)
+    if(secondNumber == NaN){
+        calcDisplay.textContent = firstNumber
+        isSecondNumber = false
+    } else if(theOperator === "/"){
+        firstNumber = firstNumber / secondNumber
+    }
+    else if(theOperator === "*"){
+        firstNumber = firstNumber * secondNumber
+    }
+    else if(theOperator === "+"){
+        firstNumber = firstNumber + secondNumber
+    }
+    else if(theOperator === "-"){
+        firstNumber = firstNumber - secondNumber
+    }
+    calcDisplay.textContent = firstNumber
+    secondNumber = ""
+    isSecondNumber = false
+}
+
+function doClear(){
+    firstNumber = ""
+    secondNumber = ""
+    theOperator = ""
+    isSecondNumber = false
+    calcDisplay.textContent = ""
 }
